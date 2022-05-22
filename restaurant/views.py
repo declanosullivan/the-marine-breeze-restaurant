857504902ff61cab432b62264b4c94997bae8931
declanosullivan <declanseer@gmail.com>
2022-05-22 19:55:19 +0000

    Add content to landing page

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 from .forms import ContactForm
 
 def contact_view(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            pass 
-    else: 
-        form = ContactForm(request)
-    return render(request, 'restaurant.html', {'form': form})
+    """ A view to return the index page """
 
+    return render(request, 'restaurant.html')
