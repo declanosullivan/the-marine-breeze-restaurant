a068dea93e84899bb49514e3ea953f1f78a85452
declanosullivan <declanseer@gmail.com>
2022-06-02 04:00:05 +0000

    Add grappelli skin to admin panel

--- marine_breeze/urls.py
+++ marine_breeze/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
  
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('bookings/', include('bookings.urls')),
